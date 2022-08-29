d0992aaf156a309d7c84adb43fc9d8163c76da14
patriicke <patrickndayambaje4@gmail.com>
2022-08-29 16:24:34 +0200

    This is the cors configurations

---
+++ config/cors/index.js
@@ -0,0 +1,17 @@
+const whitelist = require("./../origins/origins");
+
+const corsOptions = {
+  origin: (origin, callback) => {
+    if (whitelist.indexOf(origin) !== -1) {
+      callback(null, true);
+    } else {
+      callback(new Error("Not allowed by CORS"));
+    }
+  },
+  methods: ["GET", "PUT", "POST", "PATCH", "DELETE"],
+  allowedHeaders: ["Content-Type", "Authorization", "x-csrf-token"],
+  credentials: true,
+  exposedHeaders: ["*", "Authorization"]
+};
+
+module.exports = corsOptions;
